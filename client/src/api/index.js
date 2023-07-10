e8a66179436b83e7f26b794301bd6aea37fd10e9
narasimha893 <npasupuleti893@gmail.com>
2023-07-10 16:35:51 +0530

    backend URL added

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://stack-overflow-backend-wb1t.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
